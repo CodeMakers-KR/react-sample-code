cebc01a73cc033d73467b6ebab70487f687702af
Code Makers <codemakers.kr@gmail.com>
2025-04-17 21:28:27 +0900

    Create App.js

---
+++ chapter04/App.js
@@ -0,0 +1,40 @@
+function App() {
+  return (
+    <div class="wrapper">
+      <header>React Todo</header>
+      <ul className="tasks">
+        <li className="tasks-header">
+          <input id="checkall" type="checkbox" />
+          <label>Task</label>
+          <span className="due-date">Due date</span>
+          <span className="priority">Priority</span>
+        </li>
+        <li className="task-item">
+          <input id="todo_1" type="checkbox" />
+          <label htmlFor="todo_1">React Component 마스터</label>
+          <span className="due-date">2025-12-31</span>
+          <span className="priority">1</span>
+        </li>
+        <li className="task-item">
+          <input id="todo_2" type="checkbox" />
+          <label htmlFor="todo_2">React Component 마스터</label>
+          <span className="due-date">2025-12-31</span>
+          <span className="priority">1</span>
+        </li>
+      </ul>
+      <footer>
+        <input type="text" placeholder="Task" />
+        <input type="date" />
+        <select>
+          <option>우선순위</option>
+          <option value="1">높음</option>
+          <option value="2">보통</option>
+          <option value="3">낮음</option>
+        </select>
+        <button type="button">Save</button>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
