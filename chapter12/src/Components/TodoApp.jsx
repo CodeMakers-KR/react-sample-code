5f9c21722b6d80b91d1d5dca57b81c061927d374
Code Makers <codemakers.kr@gmail.com>
2025-04-18 15:24:08 +0900

    Update TodoApp.jsx

--- chapter12/src/Components/TodoApp.jsx
+++ chapter12/src/Components/TodoApp.jsx
@@ -1,12 +1,8 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import TaskList from "./TaskList";
 import TaskAppender from "./TaskAppender";
-import Alert from "./modal/Modal";
 
 export default function TodoApp() {
-  const alertRef = useRef();
-
-  const [alertMessage, setAlertMessage] = useState();
   const [todoLists, setTodoLists] = useState([
     {
       id: "item1",
@@ -35,19 +31,9 @@ export default function TodoApp() {
     <>
       <div className="wrapper">
         <header>React Todo</header>
-        <TaskList
-          todoLists={todoLists}
-          setTodoLists={setTodoLists}
-          setAlertMessage={setAlertMessage}
-          alertRef={alertRef}
-        />
+        <TaskList todoLists={todoLists} setTodoLists={setTodoLists} />
         <TaskAppender setTodoLists={setTodoLists} />
       </div>
-      <Alert ref={alertRef}>
-        <div>
-          <h3>{alertMessage}</h3>
-        </div>
-      </Alert>
     </>
   );
 }
